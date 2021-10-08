036743dd1491449d1fda0d40b3f7e15bf125144d
EneOne <89634173+EneOne@users.noreply.github.com>
2021-10-08 11:10:52 +0200

    Initial draft of toolbar (#45)
    optional

---
+++ web/src/app/expandable-table/expandable-table.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExpandableTableComponent } from './expandable-table.component';
+
+describe('ExpandableTableComponent', () => {
+  let component: ExpandableTableComponent;
+  let fixture: ComponentFixture<ExpandableTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ExpandableTableComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExpandableTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
