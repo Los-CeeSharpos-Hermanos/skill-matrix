8539634f2b74ce7f14d16af557b657d7f7d80a4d
Ene <viaregiaviaimperii@gmail.com>
2021-10-15 09:44:14 +0200

    Minor changes

--- web/src/app/admin/categories/crud/crud.component.ts
+++ web/src/app/admin/categories/crud/crud.component.ts
@@ -1,22 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { ICategory } from '../category';
+import { CategoryService } from './services/category.service';
 
-export interface Categories {
-  name: string;
-  number: number;
-  //position: number;
-  //weight: number;
-}
-
-const ELEMENT_DATA: Categories[] = [
-  {name: 'Frontend', number: 5},
-  {name: 'Backend', number: 3},
-  {name: 'Databases', number: 15},
-  {name: 'Sports', number: 8},
-  {name: 'Soft Skills', number: 7},
-  {name: 'Hobbies', number: 12}
-];
+// was here the thing
 
 @Component({
   selector: 'categories-crud',
@@ -26,24 +14,41 @@ const ELEMENT_DATA: Categories[] = [
 
 export class CRUDComponent implements OnInit {
 
+  categories: ICategory[];
+  dataSource: MatTableDataSource<ICategory>;
+  errorMessage: "";
+  pageTitle: "Skill Categories";
+
   there = "skillmatrix/categories/add";
 
 
-  displayedColumns: string[] = ['name', 'number', 'buttons'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['name', 'member', 'buttons'];
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private categoryService: CategoryService) { }
 
   goTo(path: string) {
     this.router.navigate([path]);
   }
 
   ngOnInit(): void {
+    this.loadCategories();
+    console.log(JSON.stringify(this.dataSource));
+  }
+
+  private loadCategories() {
+    this.categoryService.listCategories().subscribe({
+      next: categories => {
+        this.categories = categories;
+        console.log(categories)
+        this.dataSource = new MatTableDataSource(categories);
+      },
+      error: err => { this.errorMessage = err; console.log(err); }
+    });
   }
 
 }
