7650a4b8a0465da7f47f8870eb42939ce8ab0d23
conrado.goncalves <53683786+conradoalexsander@users.noreply.github.com>
2021-10-27 12:52:36 +0200

    Normalize styles accross the project

--- SkillMatrix/ClientApp/src/app/admin/languages/language.module.ts
+++ SkillMatrix/ClientApp/src/app/admin/languages/language.module.ts
@@ -4,18 +4,16 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AddLanguageComponent } from './components/editLanguage/add-language.component';
 import { LanguagelistComponent } from './components/languagelist/languagelist.component';
-import { LanguagesComponent } from './components/languages/languages.component';
 
 
 @NgModule({
   declarations: [
-    LanguagesComponent,
     LanguagelistComponent,
     AddLanguageComponent,
   ],
   imports: [
     RouterModule.forChild([
-      { path: 'skillmatrix/languages/:id', component: LanguagesComponent },
+      { path: 'skillmatrix/languages/:id', component: AddLanguageComponent },
       { path: 'skillmatrix/languages', component: LanguagelistComponent },
       { path: 'skillmatrix/languages/:id/add', component: AddLanguageComponent },
     ]),
