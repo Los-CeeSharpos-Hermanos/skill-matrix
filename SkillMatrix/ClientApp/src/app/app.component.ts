0117e1957e5bc8f8716ba69458887458bc491dbf
A.N. Ene <viaregiaviaimperii@gmail.com>
2021-11-01 16:37:58 +0100

    The beacons of Gondor are lit

--- SkillMatrix/ClientApp/src/app/app.component.ts
+++ SkillMatrix/ClientApp/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, Inject, OnInit, Renderer2} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+import {host} from "@angular-devkit/build-angular/src/test-utils";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  lightsOn = false;
 
   title = "web";
+
+  switchTheme(mode: boolean)
+  {
+    this.lightsOn = mode;
+    const hostClass = this.lightsOn ? 'light-theme' : 'dark-theme';
+    this.rederer.setAttribute(this.document.body, 'class', hostClass);
+  }
+
+  constructor(@Inject(DOCUMENT) private document: Document,
+              private rederer: Renderer2) {
+  }
+
+  ngOnInit()
+  {
+    this.rederer.setAttribute(this.document.body, 'class', 'dark-theme');
+  }
 
 }
 
--- SkillMatrix/ClientApp/src/app/profile/profile.component.ts
+++ SkillMatrix/ClientApp/src/app/profile/profile.component.ts
@@ -48,7 +48,6 @@ export class ProfileComponent implements OnInit {
     this.categoriesInit();
 
     this.filterInit();
-
   }
 
   categoriesInit() {
